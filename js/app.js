af3c8657ce98eecd99c978ccacf2035c6b3b2e25
Brian McAleese <bmcaleese12@gmail.com>
2016-08-10 16:40:15 +0100

    Fix syntax for setting observable

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ function HomePageViewModel()
 		$.getJSON("config/config.json" , function( data ) 
 		{
 			console.log( " loaded" );
-			self.firstName = "Loading";	
+			self.firstName("Loading" );
 		});	
 	}	
 }

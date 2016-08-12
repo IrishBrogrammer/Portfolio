f3b7536c9e4daf056aec59d8225f88825313276b
Brian McAleese <bmcaleese12@gmail.com>
2016-08-12 10:52:48 +0100

    Removed comments and redundant code

--- js/app.js
+++ js/app.js
@@ -1,13 +1,6 @@
-function AppViewModel()
-{
-	this.firstName = "Brian";
-}
-
 
 function SocialViewModel( desc , url , img )
 {
-	var self = this;
-	console.log( " Desc " + desc);
 	this.Desc = desc;
 	this.Url = url;
 	this.ImgURL = img;
@@ -25,27 +18,13 @@ function HomePageViewModel()
 	{
 		$.getJSON("config/config.json" , function( data ) 
 		{
-			console.log( " loaded" );
 			self.firstName(data.title );
-			
-			var testArray = [];
-			testArray.push( new SocialViewModel( " brian" , " 1 " , " 2 "));
-			self.Social( testArray );
-			
+		
 			self.Social( _.map( data.SocialLinks , 
 				function( v , k ) 
 				{
 					return new SocialViewModel( v.desc , v.Link, v.Image);	
-				}));_
-			
-			//self.Social( configArray );
-			
-			//self.Social( _.map( data.SocialLinks , 
-		//		function( val , key )
-		//		{
-		//			return new SocialViewModel( val.desc , val.Link, val.Image);
-		//		}	
-		//	 ));
+				}));
 		});	
 	}	
 }
